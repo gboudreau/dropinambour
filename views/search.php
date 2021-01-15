ecd894025e3e063f6191ef7407d9056341fbab9e
Guillaume Boudreau <guillaume@pommepause.com>
2021-01-14 19:35:19 -0500

    Improvement: Empty search should show an alert, not an error message

--- views/search.php
+++ views/search.php
@@ -20,8 +20,14 @@ $this->layout('/page', ['title' => $title]);
 <link href="<?php phe(Router::getAssetUrl('./css/tmdb_collection.css')) ?>" rel="stylesheet">
 <?php $this->end() ?>
 
-<h1 class="mb-3">Search results for "<?php phe(@$_REQUEST['query']) ?>"</h1>
+<?php if (empty($search_results)) : ?>
+    <div class="alert alert-primary">
+        Use the search field above to search for movies or TV shows.
+    </div>
+<?php else : ?>
+    <h1 class="mb-3">Search results for "<?php phe(@$_REQUEST['query']) ?>"</h1>
 
-<div id="search_results">
-    <?php $this->insert('/media_items', ['medias' => $search_results]) ?>
-</div>
+    <div id="search_results">
+        <?php $this->insert('/media_items', ['medias' => $search_results]) ?>
+    </div>
+<?php endif; ?>
