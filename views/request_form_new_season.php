a3fad79a3e623bcb39252329385f762c0b0f7afb
Guillaume Boudreau <guillaume@pommepause.com>
2023-09-24 09:47:33 -0400

    When requesting a new season for a show that was already requested, you can't change the path, quality or language - those should all be hidden in the form.

--- views/request_form_new_season.php
+++ views/request_form_new_season.php
@@ -46,50 +46,9 @@ use stdClass;
             <?php endforeach; ?>
         </select>
     </div>
-    <?php if (Plex::isServerAdmin() && empty($media->requested)) : ?>
-        <div class="col-auto">
-            <label class="visually-hidden" for="path-input">Path</label>
-            <select name="path" class="form-control" id="path-input">
-                <option value="">Path</option>
-                <?php foreach ($paths as $path) : ?>
-                    <option value="<?php phe($path) ?>" <?php echo_if($path == $default_path, 'selected') ?>><?php phe($path) ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-        <div class="col-auto">
-            <label class="visually-hidden" for="quality-input">Quality</label>
-            <select name="quality" class="form-control" id="quality-input">
-                <option value="">Quality</option>
-                <?php foreach ($profiles as $qp) : ?>
-                    <option value="<?php phe($qp->id) ?>" <?php echo_if($qp->id == $default_quality, 'selected') ?>><?php phe($qp->name) ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-        <div class="col-auto">
-            <label class="visually-hidden" for="language-input">Language</label>
-            <select name="language" class="form-control" id="language-input">
-                <option value="">Language</option>
-                <?php foreach (Sonarr::getLanguageProfiles() as $lp) : ?>
-                    <option value="<?php phe($lp->id) ?>" <?php echo_if($lp->id == $default_language, 'selected') ?>><?php phe($lp->name) ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-    <?php else: ?>
-        <input name="path" type="hidden" value="<?php phe($default_path) ?>">
-        <?php if (Config::get('RADARR_SIMPLIFIED_QUALITY')) : ?>
-            <div class="col-auto">
-                <label class="visually-hidden" for="quality-input">Quality</label>
-                <select name="quality" class="form-control" id="quality-input">
-                    <option value="">Choose one</option>
-                    <?php foreach (Config::get('RADARR_SIMPLIFIED_QUALITY', [], Config::GET_OPT_PARSE_AS_JSON) as $id => $name) : ?>
-                        <option value="<?php phe($id) ?>" <?php echo_if($id == $default_quality, 'selected') ?>><?php phe($name) ?></option>
-                    <?php endforeach; ?>
-                </select>
-            </div>
-        <?php else: ?>
-            <input name="quality" type="hidden" value="<?php phe($default_quality) ?>">
-        <?php endif; ?>
-    <?php endif; ?>
+    <input name="path" type="hidden" value="<?php phe($default_path) ?>">
+    <input name="quality" type="hidden" value="<?php phe($default_quality) ?>">
+    <input name="language" type="hidden" value="<?php phe($default_language) ?>">
     <div class="col-auto">
         <button class="btn btn-primary" type="submit" disabled onclick="disable_button(this); this.form.submit()">Request</button>
     </div>
